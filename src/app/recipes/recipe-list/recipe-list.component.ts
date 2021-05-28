15e6b011c33b56ef718d38f641ff7898cd199f5f
walterswoodward <walter.sekope.woodward@gmail.com>
2021-05-28 15:16:13 -0700

    Adds recipe model class containing three public properties required on instantiation (name, description, and imagePath) - Adds a recipes property of type array and adds one element to that array - an instance of the previously created Recipe model class - Builds out recipe list component html to include a New Recipe button and a list of recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Assorted Seafood and Quiche', 'Eggwhites, Baby Spinach, Chopped Red Onions, and a buttery cheddar quiche topped with shrimp, and sprinkled with dillweed, pepper, and lemon!', 'https://c.pxhere.com/photos/ab/0d/Foods_Lunch_Prawn_Prawns_Seafood_Shrimps-1622529.jpg!d'),
+  ];
 
   constructor() { }
 
